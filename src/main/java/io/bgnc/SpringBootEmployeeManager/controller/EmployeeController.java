729c540a936c97fbb5f1e34f279afbd5b24bbca6
Bugra <71045060+TheGnc@users.noreply.github.com>
2022-03-28 01:57:13 +0300

    employee service and controller testing

--- src/main/java/io/bgnc/SpringBootEmployeeManager/controller/EmployeeController.java
+++ src/main/java/io/bgnc/SpringBootEmployeeManager/controller/EmployeeController.java
@@ -1,18 +1,67 @@
 package io.bgnc.SpringBootEmployeeManager.controller;
 
+import io.bgnc.SpringBootEmployeeManager.model.Employee;
 import io.bgnc.SpringBootEmployeeManager.service.EmployeeService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
+@AllArgsConstructor
 public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-    @Autowired
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+      List<Employee> employeeList=employeeService.getAllEmployees();
+
+      return new ResponseEntity<>(employeeList,HttpStatus.OK);
     }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id")Long id){
+        Employee employee=employeeService.findEmployeeById(id);
+
+        return new ResponseEntity<>(employee,HttpStatus.OK);
+    }
+
+    @PostMapping("/addEmployee")
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+
+        Employee newEmployee = employeeService.addEmployee(employee);
+
+        /**
+         * CREATED MEANS : 201
+         */
+        return new ResponseEntity<>(newEmployee,HttpStatus.CREATED);
+
+    }
+
+    @PutMapping("/updateEmployee")
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+
+        /**
+         * OK : 200
+         * ACCEPTED:202
+         */
+        Employee updateEmployee = employeeService.updateEmployee(employee);
+        return new ResponseEntity<>(updateEmployee,HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id){
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
 }
--- src/main/java/io/bgnc/SpringBootEmployeeManager/service/EmployeeService.java
+++ src/main/java/io/bgnc/SpringBootEmployeeManager/service/EmployeeService.java
@@ -3,27 +3,29 @@ package io.bgnc.SpringBootEmployeeManager.service;
 import io.bgnc.SpringBootEmployeeManager.exceptions.EmployeeNotFoundException;
 import io.bgnc.SpringBootEmployeeManager.model.Employee;
 import io.bgnc.SpringBootEmployeeManager.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Transactional
+@AllArgsConstructor
+@Slf4j
 public class EmployeeService {
+
     private final EmployeeRepository employeeRepository;
 
-    @Autowired
-    public EmployeeService(EmployeeRepository employeeRepository) {
-        this.employeeRepository = employeeRepository;
-    }
+
 
     public Employee addEmployee(Employee employee){
 
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepository.save(employee);
-
 
 
     }
@@ -36,9 +38,9 @@ public class EmployeeService {
         return employeeRepository.findAll();
     }
 
-    public Optional<Employee> findEmployeeById(Long id){
-        return Optional.ofNullable(employeeRepository.findEmployeeById(id)
-                .orElseThrow(() -> new EmployeeNotFoundException("Employee by id " + id + " could not found")));
+    public Employee findEmployeeById(Long id){
+        return employeeRepository.findEmployeeById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee by id " + id + " could not found"));
 
     }
 
